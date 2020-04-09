58b47bc3cc7d8b35c2bfee61aeaaf98011930b26
Charlotte Mountain <charr.mt@gmail.com>
2020-04-08 21:53:15 -0700

    done for today

--- server.js
+++ server.js
@@ -1,51 +1,52 @@
-const express = require("express");
 const path = require("path");
-const fs = require("fs");
-
+const express = require("express");
 const app = express();
-var PORT = process.env.PORT || 3000;
 
-const notes = []
+
+const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
+// This middleware is responsible for constructing the
+// body property on the response object passed to our route handlers.
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+//empty for user
+const notes = []
 
-// Basic route that sends the user to notes.html
+// Routes
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "notes.html"));
 });
 
-// Basic route that sends the user to index.html
-app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-//write notes to file
-fs.appendFile("db/db.json", notes, function(err) {
-    if (err) throw err;
-    console.log("Note Added");
-});
-
+//data for current 5 tables
 app.get("/api/notes", (req, res) => {
-    return res.json(notes);
+     res.json(notes);
   });
 
+ 
+// Create New reservations - takes in JSON input
 app.post("/api/notes", (req, res) => {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body parsing middleware
     const newNote = req.body;
-  
+
     console.log(newNote);
-  
     notes.push(newNote);
-  
+
+
     res.json(newNote);
   });
 
-
 // Starts the server to begin listening
 app.listen(PORT, () => {
-    console.log("App listening on PORT " + PORT);
-  });
-  
+  console.log("App listening on PORT " + PORT);
+});
+// create another route for reservation
+// view tables & home page
+// posts for clear tables
+// posts for making reservations
+//
