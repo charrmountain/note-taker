e4600698bc0a8e891e045c693d7dedffbdc3d9fe
Charlotte Mountain <charr.mt@gmail.com>
2020-04-09 16:16:46 -0700

    post and get working

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const express = require("express");
+const fs = require("fs")
 const app = express();
 
 
@@ -18,13 +19,15 @@ app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "notes.html"));
 });
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-
 //data for current 5 tables
 app.get("/api/notes", (req, res) => {
-     res.json(notes);
+  fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+    if (err) throw err
+
+    jsonData = JSON.parse(data)
+    console.log(jsonData)
+    res.json(jsonData);
+  })
   });
 
  
@@ -32,15 +35,28 @@ app.get("/api/notes", (req, res) => {
 app.post("/api/notes", (req, res) => {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body parsing middleware
+    
     const newNote = req.body;
-
+    
     console.log(newNote);
-    notes.push(newNote);
-
-
+    
     res.json(newNote);
+    
+    notes.push(newNote);
+    
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes), 'utf8', function (err) {
+      if (err) throw err;
+      console.log('Note Added!');
+    });
+
+   return newNote;
+   
   });
 
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
 // Starts the server to begin listening
 app.listen(PORT, () => {
   console.log("App listening on PORT " + PORT);
